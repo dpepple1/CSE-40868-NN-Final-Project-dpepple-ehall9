ae356d2025c0d0733f4843fd395eda16af18f431
Derek Pepple <dpepple@nd.edu>
2023-04-05 11:05:02 -0400

    modified imagetetst to make more efficient

--- src/imagetest.py
+++ src/imagetest.py
@@ -13,6 +13,37 @@ def find_enemy(obs,i):
         threshold = 0.6
         loc = np.where( res >= threshold)
         for pt in zip(*loc[::-1]):
-            print(pt)
+            print(f'{i}', f'{pt}', end='  | ')
             cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-    cv.imwrite(f'res{i}.png',img_rgb)
+        print('')
+    #cv.imwrite(f'res{i}.png',img_rgb)
+
+
+
+class EnemyFinder():
+
+    templates = []
+
+    def __init__(self):    
+
+        #setup template images
+        for filename in os.listdir('Template images'):
+            template = cv.imread(f'Template images/{filename}', cv.IMREAD_GRAYSCALE)
+            self.templates.append(template)
+
+
+    def find_enemies(self, obs, i):
+
+        img_rgb = cv.cvtColor(obs, cv.COLOR_BGR2RGB)
+        img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
+
+        for key, template in enumerate(self.templates):
+            w, h = template.shape[::-1]
+            res = cv.matchTemplate(img_gray,template,cv.TM_CCOEFF_NORMED)
+            threshold = 0.6
+            loc = np.where( res >= threshold)
+            print(key, end = ': ')
+            for pt in zip(*loc[::-1]):
+                print(f'{i}', f'{pt}', end='  | ')
+                cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+            print('')
--- src/run_galaga.py
+++ src/run_galaga.py
@@ -1,15 +1,22 @@
 import gymnasium as gym
 import retro
 import imagetest
-env = retro.make(game="GalagaDemonsOfDeath-Nes")
+from Discretizer import GalagaDiscretizer
+from imagetest import EnemyFinder
+
+env = retro.make(game="GalagaDemonsOfDeath-Nes[A] - Copy")
+env = GalagaDiscretizer(env)
+ef = EnemyFinder()
 
 obs = env.reset()
+
 for i in range(200):
     action = env.action_space.sample()
-    print(action)
+    #print(action)
     obs, rewards, done, info = env.step(action)
-    imagetest.find_enemy(obs, i)
-    env.render()
+    #imagetest.find_enemy(obs, i)
+    ef.find_enemies(obs, i)
+    #env.render()
 
     if done:
         break
