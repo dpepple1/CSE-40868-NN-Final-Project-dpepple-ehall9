749ffb249b28139d1144176c096325b2217f8f6e
Evan Hall <ehall9@nd.edu>
2023-04-11 16:52:26 -0400

    ship finding progress

--- src/imagetest.py
+++ src/imagetest.py
@@ -36,9 +36,10 @@ class EnemyFinder():
             template = cv.imread(f'Template images/{filename}', cv.IMREAD_GRAYSCALE)
             self.templates.append(template)
 
+        self.ship = cv.imread(f'Ship Template/ship.png',cv.IMREAD_GRAYSCALE)
+
 
     def find_enemies(self, obs):
-
         img_rgb = cv.cvtColor(obs, cv.COLOR_BGR2RGB)
         img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
         points = []
@@ -54,14 +55,33 @@ class EnemyFinder():
         
         return points
 
+    def find_self(self, obs):
+        img_rgb = cv.cvtColor(obs, cv.COLOR_BGR2RGB)
+        img_gray = cv.cvtColor(img_rgb, cv.COLOR_RGB2GRAY)
+        points = []
+        w, h = self.ship.shape[::-1]
+        res = cv.matchTemplate(img_gray,self.ship,cv.TM_CCOEFF_NORMED)
+        print(res)
+        threshold = 0.6
+        loc = np.where(res>=threshold)
+        for pt in zip(*loc[::-1]):
+                cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+                points.append(pt)
+        cv.imwrite('res.png',img_rgb)
+        #return ship
+
             
     def fill_grid(self, obs):
         grid = np.zeros((self.xdiv, self.ydiv))
         points = self.find_enemies(obs)
+        #you = self.find_self(obs)
+        self.find_self(obs)
         for point in points:
             xmapped = point[0] * self.xdiv // self.xres
             ymapped = point[1] * self.ydiv // self.yres
 
             grid[xmapped][ymapped] = 1
-        
+        #xself = you[0] * self.xdiv // self.xres
+        #yself = you[1] * self.xdiv // self.xres
+        #grid[xself][yself] = 2
         return grid
