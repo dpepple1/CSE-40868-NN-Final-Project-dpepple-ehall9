6e2430787679c7f6e513b189fbeda098f6a6635d
Evan Hall <ehall9@nd.edu>
2023-05-08 22:09:44 -0400

    To Quote Maroon 5

--- src/run_galaga.py
+++ src/run_galaga.py
@@ -1,3 +1,4 @@
+import os
 import gymnasium as gym
 import retro
 import imagetest
@@ -7,29 +8,33 @@ from MLP import MLP
 import numpy as np
 import torch
 
-env = retro.make(game="GalagaDemonsOfDeath-Nes")
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+print(SCRIPT_DIR)
+retro.data.Integrations.add_custom_path(os.path.join(SCRIPT_DIR, "custom_integrations"))
+env = retro.make(game="Galaga (U)", inttype=retro.data.Integrations.ALL)
 env = GalagaDiscretizer(env)
 ef = EnemyFinder(224, 240, 22, 24)
 
 obs = env.reset()
 
-xres, yres, _ = env.observation_space.shape
-xdiv = xres // 10
-ydiv = yres // 10
+#xres, yres, _ = env.observation_space.shape
+#xdiv = xres // 10
+#ydiv = yres // 10
 
 
-model = MLP(xdiv * ydiv, 6)
 
-for i in range(5000):
+while True:
     action = env.action_space.sample()
     #print(action)
-    obs, rewards, done, info = env.step(5)
+    obs, rewards, done, info = env.step(0)
+    #print(info)
+    if rewards != 0:
+        print(rewards)
 
-    grid = ef.fill_grid(obs)
-    grid = torch.tensor(grid.flatten()).float()
+    #grid = ef.fill_grid(obs)
+    #grid = torch.tensor(grid.flatten()).float()
     #print(grid)
-    action = model(grid)
-    print(action)
+    #print(action)
 
     env.render()
 
